df9ac3006b7db26e342350172a50fc3e5ec34810
Ahmed <ahmedgouda9484@gmail.com>
2016-09-04 20:25:08 -0500

    Added spacing between stuff.

--- week1/day1/2.js
+++ week1/day1/2.js
@@ -1,14 +1,14 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function fizzBuzz() {
-  for(var i=1; i<=100; i++){
-    if (i%3 === 0){
-      if(i%5 === 0)
+  for(var i = 1; i <= 100; i++){
+    if (i % 3 === 0){
+      if(i % 5 === 0)
         console.log('FizzBuzz')
       else
         console.log('Fizz')
     }
-    else if (i%5 === 0 && i%3 !== 0)
+    else if (i % 5 === 0 && i % 3 !== 0)
       console.log('Buzz')
     else
       console.log(String(i))
